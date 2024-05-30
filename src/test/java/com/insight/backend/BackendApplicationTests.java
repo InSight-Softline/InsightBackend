3de2ce3d88c81e7224be8085867194fb68e89246
Matthes Klemt <matthes.klemt@stud.htwk-leipzig.de>
2024-05-30 18:11:54 +0200

    edit: test for Rest

--- src/test/java/com/insight/backend/BackendApplicationTests.java
+++ src/test/java/com/insight/backend/BackendApplicationTests.java
@@ -8,11 +8,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+
 class BackendApplicationTests {
 	@Autowired
 	private Calculator calculator;
+	@Autowired
 	private MockMvc mockMvc;
 	
     @Test
@@ -30,7 +34,7 @@ class BackendApplicationTests {
                 .param("zahl1", "2")
                 .param("zahl2", "3"))
                 .andExpect(status().isOk())
-                .andExpect(content().string("5"));
+                .andExpect(content().string("{\"result\":5}"));
 	}
 
 }
