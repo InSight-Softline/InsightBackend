42c61ad196f38595a9c40c80fa30b07c278389ce
OrbisK <robin.kehl@singular-it.de>
2025-01-31 17:52:08 +0100

    chore: minor changes

--- src/main/java/com/insight/backend/service/question/CreateQuestionService.java
+++ src/main/java/com/insight/backend/service/question/CreateQuestionService.java
@@ -70,7 +70,7 @@ public class CreateQuestionService {
         QuestionResponseDTO responseDTO = new QuestionResponseDTO();
         responseDTO.setId(savedQuestion.getId());
         responseDTO.setName(savedQuestion.getName());
-responseDTO.setCategory(savedQuestion.getCategory());
+        responseDTO.setCategory(savedQuestion.getCategory());
 
         return responseDTO;
     }
--- src/test/java/com/insight/backend/service/CsvImportServiceTest.java
+++ src/test/java/com/insight/backend/service/CsvImportServiceTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.ByteArrayInputStream;
